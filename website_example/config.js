2e20ec33b44d48b5ed16a8219291890970a23ddd
traaitt | TRRXITTE inc <traaitt@icloud.com>
2023-08-19 18:34:09 +0200

    Initial commit

---
+++ website_example/config.js
@@ -0,0 +1,16 @@
+var api = "https://xte.trrxitte.com/api";
+
+var email = "mine@trrxitte.com";
+var telegram = "https://t.me/mineTRRXITTE";
+var discord = "https://discordapp.com/invite/YourPool";
+var facebook = "https://www.facebook.com/<YourPoolFacebook>";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://interface.traaitt.com/block.html?hash={id}";
+// var blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
+var transactionExplorer = "https://interface.traaitt.com/transaction.html?hash={id}";
+//var transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
+
+var themeCss = "themes/custom.css";
+var defaultLang = 'en';
