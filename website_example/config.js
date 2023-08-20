c4e8a2c4048aeb7878b4fb285093c0fbcbea9266
traaitt | TRRXITTE Int., incorporate <traaitt@icloud.com>
2023-08-20 14:45:09 +0200

    //

--- website_example/config.js
+++ website_example/config.js
@@ -1,9 +1,8 @@
 var api = "https://xte.trrxitte.com/api";
 let parentCoin = "traaitt";
-var trrxitte = "https://trrxitte.com";
 var telegram = "https://t.me/mineTRRXITTE";
-var telegram1 = "https://t.me/traaitt";
 var facebook = "https://www.facebook.com/TRRXITTE";
+var email = 'https://trrxitte.com'
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
